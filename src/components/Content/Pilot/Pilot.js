51515db510ee1e8d7235050e0f8de6abe79c2c4d
MateuszStank <mateuszstank@gmail.com>
2024-04-10 11:03:06 +0200

    Pierwsze nawizanie komunikacji w obie strony z ROS2

--- src/components/Content/Pilot/Pilot.js
+++ src/components/Content/Pilot/Pilot.js
@@ -1,14 +1,93 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import ROSLIB from 'roslib';
 
 import './Pilot.scss'
 import '../ComponentStylesMain.scss'
 
+
 function Pilot() {
+    const [messages, setMesseges] = useState([]);
+    const [status, setStatus] = useState('N/A');
+
+  useEffect(() => {
+
+//Nawiązanie połączenia
+    const ros = new ROSLIB.Ros({ url: "ws://localhost:9091" });
+
+       const setStatus = (status) => {
+        const statusElement = document.getElementById("status");
+        if (statusElement) {
+            statusElement.innerHTML = status;
+        }
+    };
+
+    ros.on("connection", () => {
+        setStatus("successful");
+    });
+
+    ros.on("error", (error) => {
+        setStatus(`errored out (${error})`);
+    });
+
+    ros.on("close", () => {
+        setStatus("closed");
+    });
+
+// Tworzenie obiektu listenera do odbierania wiadomości 
+
+    const my_topic_listener = new ROSLIB.Topic({
+        ros,
+        name: "/my_topic",
+        messageType: "std_msgs/String",
+    });
+
+
+    // Odbieranie wiadomości
+    my_topic_listener.subscribe((message) => {
+        setMesseges(prevMessages => [...prevMessages, message.data]);
+    });
+
+// Tworzenie obiektu publishera do nadawania wiadomości 
+
+const my_topic_publisher = new ROSLIB.Topic({
+    ros,
+    name: "/publishing_topic",
+    messageType: "std_msgs/String",
+})
+    // Dodanie obsługi kliknięcia przycisku "UP"
+    const handelUpButtonClick = () => {
+        const helloMessage = new ROSLIB.Message({
+            data: 'Naciśnięte UP'
+        });
+        my_topic_publisher.publish(helloMessage);
+    };
+
+    const upButton = document.getElementById("up-button");
+    upButton.addEventListener("click", handelUpButtonClick);
+
+//Zakończenie fukcji - zakończenie subskrybcji listenerów po odmontowywaniu komponentu
+
+    return () => {
+        my_topic_listener.unsubscribe();
+        upButton.removeEventListener("click", handelUpButtonClick)
+    };
+}, []);
+  
+
   return (
     <div className="pilot-content"> 
       <h1>Pilocik</h1>
+      <button id="up-button" className="arrow-button">UP</button>
+      <div>
+          <p>Connection: <span id="status">N/A</span></p>
+          <p><code>/my_topic</code> messages received: 
+            <ul>{messages.map((msg, index) => <li key ={index}>{msg}</li>)}</ul>
+        </p>
+      </div>
     </div>
   );
 }
+
+
 
 export default Pilot
